535c574eca01829fe99c5f8cb400e786b82443e0
Anna <anna.2002a@yandex.ru>
2024-05-16 10:28:22 +0300

    posts with pictures

--- sport_auth/app.py
+++ sport_auth/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes import auth_routes, post_routes, user_routes, team_routes
+import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
 app.secret_key = 'qwerty'
 
 auth_routes.init_auth_routes(app)
@@ -11,6 +15,10 @@ post_routes.init_post_routes(app)
 user_routes.init_user_routes(app)
 
 # team_routes.create_teams()
+
+# @app.route('/uploads/<path:filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sport_auth/routes/user_routes.py
+++ sport_auth/routes/user_routes.py
@@ -10,17 +10,54 @@ def init_user_routes(app):
             return jsonify({'status': 200, 'name': name})
         return jsonify({'status': 401, 'message': 'Unauthorized', 'name': 'test'})
 
-    @app.route('/profile')
+# ФИ, рост, вес, почта, аватарка
+
+    @app.route('/profile', methods=['GET'])
     def profile():
         if 'loggedin' in session:
-            surname = session['surname']
-            name = session['name']
-            age = session['age']
-            height = session['height']
-            weight = session['weight']
-            return render_template({'status': 200, 'surname': surname, 'name': name, 'age': age, 'height': height, 'weight': weight})
-        return jsonify({'status': 401, 'message': 'Unauthorized'})
+            user_id = session.get('id')
+            try:
+                user_profile = execute_query('SELECT * FROM users WHERE id = %s', (user_id,))
+                if user_profile:
+                    profile_data = {
+                        'id': user_profile[0],
+                        'lastName': user_profile[1],
+                        'firstName': user_profile[2],
+                        # 'email': user_profile['email'],
+                        # 'height': user_profile['height'],
+                        # 'weight': user_profile['weight'],
+                        # 'activity': [{'type': 'pool', 'color': 'blue', 'time': 16, 'calories': 8500}],  # заглушка для активности
+                        # 'team': 1,  # заглушка для команды
+                        # 'league': "gold",  # заглушка для лиги
+                        # 'avatar': session.get('avatar', 'default_avatar_url')  # заглушка для аватара
+                    }
+                    return jsonify({'status': 200, 'profile': profile_data})
+                else:
+                    return jsonify({'status': 404, 'message': 'User not found'})
+            except Exception as e:
+                print("Error fetching user profile:", e)
+                return jsonify({'status': 500, 'message': 'Internal server error'})
+        else:
+            return jsonify({'status': 401, 'message': 'Unauthorized'})
 
+    # @app.route('/profile', methods=['GET'])
+    # def profile():
+    #     if 'loggedin' in session:
+    #         profile_data = {
+    #             'id': session.get('id'),
+    #             'lastName': session.get('lastName'),  # Проверьте ключ 'lastName'
+    #             'firstName': session.get('firstName'),  # Проверьте ключ 'firstName'
+    #             'email': session.get('email'),
+    #             'height': session.get('height'),
+    #             'weight': session.get('weight'),
+    #             'activity': [{'type': 'pool', 'color': 'blue', 'time': 16, 'calories': 8500}],
+    #             'team': 1,
+    #             'league': "gold",
+    #         }
+    #         return jsonify({'status': 200, 'profile': profile_data})
+    #     return jsonify({'status': 401, 'message': 'Unauthorized'})
+
+    # отдельно изменить рост, вес. отдельно аватарка ФИ почта. отдельно прогресс? цель
     @app.route('/edit_person_data', methods=['POST'])  # не готово
     def edit_person_data():
         if 'loggedin' in session:
