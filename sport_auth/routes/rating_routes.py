9a7bee975c6518edcb8a9dcc76d6bb144f56364f
Anna <anna.2002a@yandex.ru>
2024-05-29 15:33:33 +0300

    edit profile

--- sport_auth/routes/rating_routes.py
+++ sport_auth/routes/rating_routes.py
@@ -3,45 +3,50 @@ from database import execute_query
 from routes.auth_routes import session
 
 def init_rating_routes(app):
-    @app.route('/users_leaderboard', methods=['GET'])
-    def get_users_leaderboard():
+    @app.route('/participants-rating', methods=['GET'])
+    def participants():
         results = execute_query("""
             SELECT 
-                users.surname, users.name, users.points, 
+                users.id, users.surname, users.name, users.points, 
                 teams.name
             FROM users
             JOIN teams ON users.team_id = teams.id
-            GROUP BY users.id
-            ORDER BY points DESC;
+            GROUP BY users.id, users.surname, users.name, users.points, teams.name
+            ORDER BY users.points DESC;
         """, fetchall=True)
 
         leaderboard = []
         for row in results:
             leaderboard.append({
-                'surname': row[0],
-                'name': row[1],
-                'points': row[2],
-                'team': row[3]
+                'id': row[0],
+                'lastName': row[1],
+                'firstName': row[2],
+                'progress': row[3],
+                'team': row[4]
             })
         return jsonify({'status': 200, 'message': 'Users rating created successfully', 'leaderboard': leaderboard})
 
-    @app.route('/teams_leaderboard', methods=['GET'])
-    def get_teams_leaderboard():
+    @app.route('/teams-rating', methods=['GET'])
+    def teams():
         results = execute_query("""
-            SELECT
-                teams.name, teams.points, 
-                COUNT(users.team_id)
-            FROM teams JOIN users ON teams.id = users.team_id
-            GROUP BY teams.id
-            ORDER BY teams.points DESC;
-        """, fetchall=True)
+                   SELECT
+                       teams.id, teams.name, teams.points as points, 
+                       COUNT(users.id) as members
+                   FROM teams
+                   JOIN users ON teams.id = users.team_id
+                   GROUP BY teams.id
+                   ORDER BY points DESC;
+               """, fetchall=True)
+        # SUM(users.points) as points,
+
         # count_users = execute_query("SELECT COUNT(*) FROM users WHERE team_id", fetchall=True)
 
         leaderboard = []
         for row in results:
             leaderboard.append({
-                'name': row[0],
-                'points': row[1],
-                'count_users': row[2]
+                'id': row[0],
+                'name': row[1],
+                'totalProgress': row[2],
+                'members': row[3]
             })
         return jsonify({'status': 200, 'message': 'Teams rating created successfully', 'leaderboard': leaderboard})
--- sport_auth/routes/user_routes.py
+++ sport_auth/routes/user_routes.py
@@ -17,7 +17,7 @@ def init_user_routes(app):
     @app.route('/profile', methods=['GET'])
     def profile():
         if 'loggedin' in session:
-            user_id = session.get('id')
+            user_id = session['id']
             try:
                 user_profile = execute_query('SELECT * FROM users WHERE id = %s', (user_id,))
                 if user_profile:
@@ -28,6 +28,8 @@ def init_user_routes(app):
                         'email': user_profile[4],
                         'height': user_profile[8],
                         'weight': user_profile[9],
+                        'avatar': f"http://localhost:5000/avatars/{user_profile[12]}" if user_profile[12] else "https://i.pinimg.com/736x/19/dd/ac/19ddacef8e14946b73248fe5b20338b0.jpg"
+                        # 'avatar': user_profile[12],
                         # 'activity': [{'type': 'pool', 'color': 'blue', 'time': 16, 'calories': 8500}],  # заглушка для активности
                         # 'team': 1,  # заглушка для команды
                         # 'league': "gold",  # заглушка для лиги
@@ -48,19 +50,39 @@ def init_user_routes(app):
     @app.route('/edit_person_data', methods=['POST'])  # не готово
     def edit_person_data():
         if 'loggedin' in session:
-            user_id = session.get('id')
+            user_id = session['id']
             data = request.json
             age = data.get('age')
             height = data.get('height')
             weight = data.get('weight')
 
-            try:
-                execute_query('UPDATE users SET age = %s, height = %s, weight = %s WHERE id = %s',
-                              (age, height, weight, user_id,))
-                return jsonify({'status': 200, 'message': 'User data updated successfully'})
-            except Exception as e:
-                print("Error updating user data:", e)
-                return jsonify({'status': 500, 'message': 'Internal server error'})
+            print("height", height)
+
+            query_args = []
+            update_fields = []
+            if age is not None:
+                query_args.append(age)
+                update_fields.append('age = %s')
+            if height is not None:
+                query_args.append(height)
+                update_fields.append('height = %s')
+            if weight is not None:
+                query_args.append(weight)
+                update_fields.append('weight = %s')
+
+            if update_fields:
+                query_args.append(user_id)
+                update_fields = ', '.join(update_fields)
+                query = f"UPDATE users SET {update_fields} WHERE id = %s"
+                try:
+                    execute_query(query, tuple(query_args), update=True)
+                    return jsonify({'status': 200, 'message': 'User data updated successfully'})
+                except Exception as e:
+                    print("Error updating user data:", e)
+                    return jsonify({'status': 500, 'message': 'Internal server error'})
+            else:
+                return jsonify({'status': 400, 'message': 'No data provided for update'})
+
         return jsonify({'status': 401, 'message': 'Unauthorized'})
 
     @app.route('/logout', methods=['POST'])
