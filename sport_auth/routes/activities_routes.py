c5020ba70afedc55bc28759ac2791d99b3a1a613
Anna <anna.2002a@yandex.ru>
2024-05-31 09:16:28 +0300

    challenges

--- sport_auth/routes/activities_routes.py
+++ sport_auth/routes/activities_routes.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from database import execute_query
 from routes.auth_routes import session
 import datetime
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 
 def init_activities_routes(app):
     @app.route('/activities/week', methods=['GET'])
@@ -46,8 +46,8 @@ def init_activities_routes(app):
     def activities_month():
         user_id = session['id']
         results = execute_query("""
-            SELECT 
-                activities.tag, activities.name, activities.color, 
+            SELECT
+                activities.tag, activities.name, activities.color,
                 feeds.time_beginning, feeds.time_ending
             FROM activities
             JOIN feeds ON activities.id = feeds.activity_id
@@ -74,6 +74,40 @@ def init_activities_routes(app):
                 'average': average_time}
             activities.append(activity)
         return jsonify({'status': 200, 'activities': activities})
+
+    # @app.route('/activities/month', methods=['GET'])
+    # def activities_month():
+    #     user_id = session['id']
+    #     results = execute_query("""
+    #         SELECT
+    #             activities.tag, activities.name, activities.color,
+    #             SUM(EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning)) / 60) AS total_minutes
+    #         FROM activities
+    #         JOIN feeds ON activities.id = feeds.activity_id
+    #         WHERE feeds.author_id = %s AND feeds.time_of_publication >= NOW() - INTERVAL '1 MONTH'
+    #         GROUP BY activities.tag, activities.name, activities.color
+    #     """, (user_id,), fetchall=True)
+    #
+    #     activities = []
+    #     for row in results:
+    #         total_minutes = row[3]
+    #
+    #         # Вычисляем часы и минуты из общего времени в минутах
+    #         hours = int(total_minutes // 60)
+    #         minutes = int(total_minutes % 60)
+    #
+    #         # Форматируем время для отображения
+    #         time_str = f"{hours:02}:{minutes:02}"
+    #
+    #         activity = {
+    #             'tag': row[0],
+    #             'type': row[1],
+    #             'color': row[2],
+    #             'time': time_str,
+    #         }
+    #         activities.append(activity)
+    #
+    #     return jsonify({'status': 200, 'activities': activities})
 
     @app.route('/activities/all', methods=['GET'])
     def activities_all():
