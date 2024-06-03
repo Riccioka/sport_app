8def548f9d4e65f5b5c730c7003e55bc1fc0ffba
Anna <anna.2002a@yandex.ru>
2024-06-03 13:54:26 +0300

    activity history

--- sport_auth/routes/activities_routes.py
+++ sport_auth/routes/activities_routes.py
@@ -10,134 +10,128 @@ def init_activities_routes(app):
         user_id = session['id']
         print("user_id", user_id)
         results = execute_query("""
-            SELECT 
-                activities.tag, activities.name, activities.color, 
-                feeds.time_beginning, feeds.time_ending
+            SELECT
+                activities.tag, activities.name, activities.color,
+                SUM(
+                    CASE
+                        WHEN feeds.time_beginning <= feeds.time_ending THEN
+                            EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning)) / 60
+                        ELSE
+                            EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning + INTERVAL '1 day')) / 60
+                    END
+                ) AS total_minutes
             FROM activities
             JOIN feeds ON activities.id = feeds.activity_id
-            WHERE feeds.author_id = %s AND feeds.time_of_publication >= NOW() - INTERVAL '1 WEEK'
-            GROUP BY activities.tag, activities.name, activities.color, feeds.time_beginning, feeds.time_ending
+            WHERE feeds.author_id = 9 AND feeds.time_of_publication >= NOW() - INTERVAL '1 WEEK'
+            GROUP BY activities.tag, activities.name, activities.color
         """, (user_id,), fetchall=True)
-
 
         activities = []
         for row in results:
-            time_beginning_obj = datetime.strptime(str(row[3]), '%H:%M:%S').time()
-            time_ending_obj = datetime.strptime(str(row[4]), '%H:%M:%S').time()
+            total_minutes = row[3]
+            print("total_minutes ", total_minutes)
 
-            time_delta = datetime.combine(datetime.min, time_ending_obj) - datetime.combine(datetime.min,
-                                                                                            time_beginning_obj)
+            hours = int(total_minutes // 60)
+            minutes = int(total_minutes % 60)
 
-            average_time = round((time_delta.total_seconds() / 7) / 60)
-            hours = time_delta.seconds // 3600
-            minutes = (time_delta.seconds % 3600) // 60
+            time_str = f"{hours:02}:{minutes:02}"
+
+            average_time = round(total_minutes / 7)
 
             activity = {
                 'tag': row[0],
                 'type': row[1],
                 'color': row[2],
-                'time': f"{hours:02}:{minutes:02}",
+                'time': time_str,
                 'average': average_time}
             activities.append(activity)
+            print('act', activity)
 
         return jsonify({'status': 200, 'activities': activities})
+
+
 
     @app.route('/activities/month', methods=['GET'])
     def activities_month():
         user_id = session['id']
+
         results = execute_query("""
             SELECT
                 activities.tag, activities.name, activities.color,
-                feeds.time_beginning, feeds.time_ending
+                SUM(
+                    CASE
+                        WHEN feeds.time_beginning <= feeds.time_ending THEN
+                            EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning)) / 60
+                        ELSE
+                            EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning + INTERVAL '1 day')) / 60
+                    END
+                ) AS total_minutes
             FROM activities
             JOIN feeds ON activities.id = feeds.activity_id
             WHERE feeds.author_id = %s AND feeds.time_of_publication >= NOW() - INTERVAL '1 MONTH'
-            GROUP BY activities.tag, activities.name, activities.color, feeds.time_beginning, feeds.time_ending
+            GROUP BY activities.tag, activities.name, activities.color
         """, (user_id,), fetchall=True)
 
         activities = []
         for row in results:
-            time_beginning_obj = datetime.strptime(str(row[3]), '%H:%M:%S').time()
-            time_ending_obj = datetime.strptime(str(row[4]), '%H:%M:%S').time()
+            total_minutes = row[3]
+            print("total_minutes ", total_minutes)
 
-            time_delta = datetime.combine(datetime.min, time_ending_obj) - datetime.combine(datetime.min,
-                                                                                            time_beginning_obj)
-            average_time = round((time_delta.total_seconds() / 30) / 60)  # важно ли какой месяц и сколько точно дней?
-            hours = time_delta.seconds // 3600
-            minutes = (time_delta.seconds % 3600) // 60
+            hours = int(total_minutes // 60)
+            minutes = int(total_minutes % 60)
+
+            time_str = f"{hours:02}:{minutes:02}"
+            average_time = round(total_minutes / 30) # важно ли какой месяц и сколько точно дней?
 
             activity = {
                 'tag': row[0],
                 'type': row[1],
                 'color': row[2],
-                'time': f"{hours:02}:{minutes:02}",
+                'time': time_str,
                 'average': average_time}
+
             activities.append(activity)
+
         return jsonify({'status': 200, 'activities': activities})
 
-    # @app.route('/activities/month', methods=['GET'])
-    # def activities_month():
-    #     user_id = session['id']
-    #     results = execute_query("""
-    #         SELECT
-    #             activities.tag, activities.name, activities.color,
-    #             SUM(EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning)) / 60) AS total_minutes
-    #         FROM activities
-    #         JOIN feeds ON activities.id = feeds.activity_id
-    #         WHERE feeds.author_id = %s AND feeds.time_of_publication >= NOW() - INTERVAL '1 MONTH'
-    #         GROUP BY activities.tag, activities.name, activities.color
-    #     """, (user_id,), fetchall=True)
-    #
-    #     activities = []
-    #     for row in results:
-    #         total_minutes = row[3]
-    #
-    #         # Вычисляем часы и минуты из общего времени в минутах
-    #         hours = int(total_minutes // 60)
-    #         minutes = int(total_minutes % 60)
-    #
-    #         # Форматируем время для отображения
-    #         time_str = f"{hours:02}:{minutes:02}"
-    #
-    #         activity = {
-    #             'tag': row[0],
-    #             'type': row[1],
-    #             'color': row[2],
-    #             'time': time_str,
-    #         }
-    #         activities.append(activity)
-    #
-    #     return jsonify({'status': 200, 'activities': activities})
 
     @app.route('/activities/all', methods=['GET'])
     def activities_all():
         user_id = session['id']
         results = execute_query("""
-            SELECT 
-                activities.tag, activities.name, activities.color, 
-                feeds.time_beginning, feeds.time_ending
-            FROM activities
-            JOIN feeds ON activities.id = feeds.activity_id
-            WHERE feeds.author_id = %s
-            GROUP BY activities.tag, activities.name, activities.color, feeds.time_beginning, feeds.time_ending
-        """, (user_id,), fetchall=True)
+                    SELECT
+                        activities.tag, activities.name, activities.color,
+                        SUM(
+                            CASE
+                                WHEN feeds.time_beginning <= feeds.time_ending THEN
+                                    EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning)) / 60
+                                ELSE
+                                    EXTRACT(EPOCH FROM (feeds.time_ending - feeds.time_beginning + INTERVAL '1 day')) / 60
+                            END
+                        ) AS total_minutes
+                    FROM activities
+                    JOIN feeds ON activities.id = feeds.activity_id
+                    WHERE feeds.author_id = %s
+                    GROUP BY activities.tag, activities.name, activities.color
+                """, (user_id,), fetchall=True)
 
 
         activities = []
         for row in results:
-            time_beginning_obj = datetime.strptime(str(row[3]), '%H:%M:%S').time()
-            time_ending_obj = datetime.strptime(str(row[4]), '%H:%M:%S').time()
+            total_minutes = row[3]
+            print("total_minutes ", total_minutes)
 
-            time_delta = datetime.combine(datetime.min, time_ending_obj) - datetime.combine(datetime.min,
-                                                                                            time_beginning_obj)
-            average_time = round((time_delta.total_seconds() / 7) / 60)
-            hours = time_delta.seconds // 3600
-            minutes = (time_delta.seconds % 3600) // 60
+            hours = int(total_minutes // 60)
+            minutes = int(total_minutes % 60)
+
+            time_str = f"{hours:02}:{minutes:02}"
 
             activity = {
                 'tag': row[0],
                 'type': row[1],
                 'color': row[2],
-                'time': f"{hours:02}:{minutes:02}"}
+                'time': time_str,
+            }
             activities.append(activity)
+
         return jsonify({'status': 200, 'activities': activities})
